7d6570b90ca9cfb9d4a1490d5b204a5d89821088
Jordan Shayo <jordanhildebrand2012@gmail.com>
2024-07-05 23:58:31 +0100

    unit testing for finding one object by id plus refactoring

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -5,10 +5,12 @@ import {
 } from "@angular/common/http/testing";
 import { CoursesService } from "./courses.service";
 import { COURSES } from "../../../../server/db-data";
+import { Course } from "../model/course";
 
 describe("CourseService", () => {
   let courseService: CoursesService,
     httpTestingController: HttpTestingController;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
@@ -18,6 +20,7 @@ describe("CourseService", () => {
     courseService = TestBed.inject(CoursesService);
     httpTestingController = TestBed.inject(HttpTestingController);
   });
+
   it("should retrieve all courses", () => {
     courseService.findAllCourses().subscribe((courses) => {
       expect(courses).toBeTruthy("No courses returned");
@@ -31,5 +34,42 @@ describe("CourseService", () => {
     expect(req.request.method).toEqual("GET");
 
     req.flush({ payload: Object.values(COURSES) });
+  });
+
+  it("should find a course by id", () => {
+    courseService.findCourseById(12).subscribe((course) => {
+      expect(course).toBeTruthy();
+      expect(course.id).toBe(12);
+    });
+
+    const req = httpTestingController.expectOne("/api/courses/12");
+
+    expect(req.request.method).toEqual("GET");
+
+    req.flush(COURSES[12]);
+  });
+
+  it("should save the course data", () => {
+    const changes: Partial<Course> = {
+      titles: { description: "Testing Course" },
+    };
+
+    courseService.saveCourse(12, changes).subscribe((course) => {
+      expect(course.id).toBe(12);
+    });
+
+    const req = httpTestingController.expectOne("/api/courses/12");
+
+    expect(req.request.method).toEqual("PUT");
+
+    expect(req.request.body.titles.description).toEqual(
+      changes.titles.description
+    );
+
+    req.flush({ ...COURSES[12], ...changes });
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 });
